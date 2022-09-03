63fa6a246d89688fba40af7d1fa0ed7890ef5bd4
Lory <lorebitryo@gmail.com>
2022-09-03 18:39:56 +0200

    cleaning

--- saveCookies.js
+++ saveCookies.js
@@ -1,8 +1,16 @@
+const ONE_DAY_MILLISECONDS = 24 * 60 * 60 * 1000;
 const saveButton = document.getElementById("saveButton");
 
 saveButton.addEventListener("click", () => {
   let cookieName = document.getElementById("name").value;
   let cookieValue = document.getElementById("value").value;
-  console.log(cookieName, cookieValue);
-  document.cookie = `${cookieName}=${cookieValue}`;
+  setCookies(cookieName, cookieValue, 1);
 });
+
+const setCookies = (cookieName, cookieValue, days) => {
+  const expirationDate = new Date();
+  expirationDate.setTime(
+    expirationDate.getTime() + days * ONE_DAY_MILLISECONDS
+  );
+  document.cookie = `${cookieName}=${cookieValue}; expires=${expirationDate}`;
+};
--- viewCookies.js
+++ viewCookies.js
@@ -1,8 +1,3 @@
 const divSavedCookies = document.getElementById("savedCookiesDiv");
-console.log(divSavedCookies);
-console.log(document.cookie);
-
 let allCookies = document.cookie.split(";");
-console.log(allCookies);
-
 divSavedCookies.innerHTML = allCookies;
